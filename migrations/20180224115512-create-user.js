63f09e1c33f08866e75b35219ab2a923d4fafb95
tuber <btu@nocode.com>
2018-02-25 12:55:48 +0800

    [tuber] #n/a create user table

--- migrations/20180224115512-create-user.js
+++ migrations/20180224115512-create-user.js
@@ -4,8 +4,11 @@ module.exports = {
   up: function (queryInterface, Sequelize) {
     return queryInterface.createTable('user', {
       id: { type: Sequelize.INTEGER, autoIncrement: true, unique: true, primaryKey: true },
-      name: { type: Sequelize.STRING(30), unique: true },
-      phone: { type: Sequelize.INTEGER, unique: true },
+      openId: { type: Sequelize.STRING, unique: true, allowNull: false, field: 'open_id' },
+      nickName: { type: Sequelize.STRING, field: 'nick_name' },
+      guest: { type: Sequelize.STRING },
+      avatarUrl: { type: Sequelize.STRING, field: 'avatar_url' },
+      isAccept: { type: Sequelize.BOOLEAN, field: 'is_accept' },
       createdAt: { type: Sequelize.DATE, field: 'created_at' },
       updatedAt: { type: Sequelize.DATE, field: 'updated_at' },
     });
