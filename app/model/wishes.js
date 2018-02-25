ccf94e6b9f0752b8b96570b34bfda31183845b16
tuber <btu@nocode.com>
2018-02-25 20:36:48 +0800

    [tuber] #n/a update wishes table

--- app/model/wishes.js
+++ app/model/wishes.js
@@ -3,7 +3,7 @@ module.exports = app => {
 
   const schema = {
     id: { type: Sequelize.INTEGER, autoIncrement: true, unique: true, primaryKey: true },
-    openId: { type: Sequelize.STRING, unique: true, allowNull: false, field: 'open_id' },
+    openId: { type: Sequelize.STRING, allowNull: false, field: 'open_id' },
     content: { type: Sequelize.STRING },
     createdAt: { type: Sequelize.DATE, field: 'created_at' },
     updatedAt: { type: Sequelize.DATE, field: 'updated_at' },
--- migrations/20180225083306-create-wishes.js
+++ migrations/20180225083306-create-wishes.js
@@ -4,7 +4,7 @@ module.exports = {
   up: function (queryInterface, Sequelize) {
     return queryInterface.createTable('wishes', {
       id: { type: Sequelize.INTEGER, autoIncrement: true, unique: true, primaryKey: true },
-      openId: { type: Sequelize.STRING, unique: true, allowNull: false, field: 'open_id' },
+      openId: { type: Sequelize.STRING, allowNull: false, field: 'open_id' },
       content: { type: Sequelize.STRING },
       createdAt: { type: Sequelize.DATE, field: 'created_at' },
       updatedAt: { type: Sequelize.DATE, field: 'updated_at' },
